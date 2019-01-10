ea3c115d0ccf765de41a1fae17759941c4448065
kong.zeya <kong.zeya@gmail.com>
2019-01-10 11:58:05 -0600

    add google word2vec

--- word2vecDemo.py
+++ word2vecDemo.py
@@ -1,13 +1,34 @@
 from gensim.models import KeyedVectors
 from gensim.scripts.glove2word2vec import glove2word2vec
+import time
 
-# glove_input_file = 'glove.6B.100d.txt'
-# word2vec_output_file = 'glove.6B.100d.txt.word2vec'
-# glove2word2vec(glove_input_file, word2vec_output_file)
 
-# load the Stanford GloVe model
-filename = 'glove.6B.100d.txt.word2vec'
-model = KeyedVectors.load_word2vec_format(filename, binary=False)
-# calculate: (king - man) + woman = ?
-result = model.most_similar(positive=['woman', 'king'], negative=['man'], topn=1)
-print(result)
+def start():
+    run_google_word2vec()
+
+
+def run_google_word2vec():
+    start = time.time()
+    filename = 'GoogleNews-vectors-negative300.bin'
+    model = KeyedVectors.load_word2vec_format(filename, binary=True)
+    # result = model.most_similar(positive=['woman', 'king'], negative=['man'], topn=1)
+    print(model['man'])
+    end = time.time()
+    print('time:', end-start)
+
+
+def run_glove_word2vec():
+    # glove_input_file = 'glove.6B.100d.txt'
+    # word2vec_output_file = 'glove.6B.100d.txt.word2vec'
+    # glove2word2vec(glove_input_file, word2vec_output_file)
+
+    # load the Stanford GloVe model
+    filename = 'glove.6B.100d.txt.word2vec'
+    model = KeyedVectors.load_word2vec_format(filename, binary=False)
+    # calculate: (king - man) + woman = ?
+    # result = model.most_similar(positive=['woman', 'king'], negative=['man'], topn=1)
+    print(model['king'])
+
+
+if __name__ == '__main__':
+    start()
