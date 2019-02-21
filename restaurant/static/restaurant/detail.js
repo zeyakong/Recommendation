b600fe28c5a0115ce715902937a3dae26b86bed4
kong.zeya <kong.zeya@gmail.com>
2019-02-21 00:47:31 -0600

    Redesign the GUI

---
+++ restaurant/static/restaurant/detail.js
@@ -0,0 +1,5 @@
+function fresh_stars_number() {
+    var value = $('#stars_range').val();
+    // alert(value);
+    $('#stars_number').text(value);
+}
---
+++ restaurant/templatetags/random_number.py
@@ -0,0 +1,16 @@
+import random
+from django import template
+
+register = template.Library()
+
+
+@register.simple_tag
+def random_int(a, b=None):
+    if b is None:
+        a, b = 0, a
+    return random.randint(a, b)
+
+
+@register.filter
+def get_random(list):
+    return random.choice(list)
